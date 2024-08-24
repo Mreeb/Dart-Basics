ba81adee7bab69f19b6d543a40a105e8b1a0dc92
Muhammad Areeb Khan <103059817+Mreeb@users.noreply.github.com>
2024-08-24 08:25:17 -0700

    Create TypeAnnotations.dart

---
+++ DartFiles/TypeAnnotations.dart
@@ -0,0 +1,22 @@
+void main() {
+  String name = "mario";
+  name = "Areeb";
+  print(name);
+  
+  int age = 25;
+  print(age);
+  
+  bool isOpen = true;
+  print(isOpen);
+  
+  double averageRating = 7;
+  averageRating = 7.5;
+  print(averageRating);
+  
+  
+  //Null safety
+  
+  int? points;
+  points = 31;
+  print(points);
+}
